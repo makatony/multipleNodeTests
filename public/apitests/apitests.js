4549b03e83fee750026d6ec4d459e4cfd782c5c0
makatony <makatony@gmail.com>
2017-01-08 16:29:05 +0100

    added text summarization (using preexisting library) upon clicking on the news title

--- public/apitests/apitests.js
+++ public/apitests/apitests.js
@@ -2,37 +2,53 @@ var socket;
 
 $(function () {
 
+	//search input
 	$("<input>", {type: "text",val: 'Fort Lauderdale',id: 'queryString'}).appendTo($('body'));
 	$("<input>", {type: "submit"}).bind("click", getNews).appendTo($('body'));
+	
+	//news table
 	$("<p>", {id: "newsSource"}).appendTo($('body'));
-	$("<div>", {id: "newsResult"}).appendTo($('body'));
-	$("<div>", {id: "jsonResult"}).appendTo($('body'));
+	$("<div>", {id: "newsResult"}).css("display","none").appendTo($('body'));
+	
+	//json textarea
+	$("<div>", {id: "jsonResult"}).appendTo($('#newsResult'));
 	$("<a>", {id:"jsonresultDisplayToggle", href:"#", text: "display/hide JSON response"}).appendTo($('#jsonResult'));
 	$("<textarea>", {id: "jsonresultText", width:'100%', height:400}).css("display","none").appendTo($('#jsonResult'));
 
 	$("<style type='text/css'> table{ border-collapse:collapse; } th, td { border: 1px solid ; } </style>").appendTo("head");
 
 	
+	//popup for summarized text
+	$("<div>", {id: "dialog", text:""}).appendTo($('body'));
+	$("<p>", {id: "d_summaryText", text:""}).appendTo($('#dialog'));
+	$("<a>", {id: "d_OrigTextToggle", href:"#", text:"display/hide original unsummarized text"}).appendTo($('#dialog'));
+	$("<p>", {id: "d_originalText", text:""}).css("display","none").appendTo($('#dialog'));
+	$( "#dialog" ).dialog( {width:700, modal:true } );
+	$( "#dialog" ).dialog( "close" );
+	
+	
+	
+	
+	//event handlers
 	$("#jsonresultDisplayToggle").bind("click", function () {
 		if ($("#jsonresultText").css("display") == "none") $("#jsonresultText").css("display","block");
 		else $("#jsonresultText").css("display","none");
 	});
 	
+$("#d_OrigTextToggle").bind("click", function () {
+		if ($("#d_originalText").css("display") == "none") $("#d_originalText").css("display","block");
+		else $("#d_originalText").css("display","none");
+	});
+	
+
+	
 })
 
 function createNewsTable(result) {
-	$('<table>', {
-		id: "newsTable"
-	}).append($('<tr>')
-		.append($('<th>', {
-				text: "News title"
-			}))
-		.append($('<th>', {
-				text: "NLC Sentiment (Confidence)"
-			}))
-		.append($('<th>', {
-				text: "AFINN Sentiment (score)"
-			}))).appendTo($('#newsResult'))
+	$('<table>', {id: "newsTable"}).append($('<tr>')
+		.append($('<th>', {	text: "News title"	}))
+		.append($('<th>', {	text: "NLC Sentiment (Confidence)"	}))
+		.append($('<th>', {	text: "AFINN Sentiment (score)"	}))).appendTo($('#newsResult'))
 
 	for (var i = 0; i < result.news.length; i++) {
 		var title = result.news[i].source.enriched.url.title;
@@ -50,11 +66,12 @@ function createNewsTable(result) {
 			.append($('<td>', {html: title}))
 			.append($('<td>', {text: NLCsentiment.top_class + " (" + NLCsentimentConfidence + ")"}))
 			.append($('<td>', {text: afinnSentiment + " (" + afinn.totalScore + ")"	})))
-
 	}
 	
 	$("<p>", { text: "Hover over the titles in the table to see the AFINN scores"}).appendTo($('#newsResult'));
+	$("<p>", { text: "Click on the titles to see summarized content of the article"}).appendTo($('#newsResult'));
 	
+	$("#newsResult").css("display","block");
 	
 	$(".afinnHover").tooltip({
 		items: "span",
@@ -73,7 +90,46 @@ function createNewsTable(result) {
 
 		$(this).text(result.news[newsId].source.enriched.url.title);
 	});
+	
+	
+	$(".afinnHover").bind("click", function () {
+		var element = $(this);
+		var newsId = element.parent().parent().attr("newsId");
+		
+		var htmlContent = "Summary text:<br>"+result.news[newsId].source.summary.summary;
+		$("#dialog > #d_summaryText").html(htmlContent);
+		htmlContent = "<br>Original text:<br>"+result.news[newsId].source.enriched.url.text;
+		$("#dialog > #d_originalText").html(htmlContent);
+		
+		$("#dialog").dialog({ title: result.news[newsId].source.enriched.url.title });
+	});
+	
 }
+
+
+// #######################
+// #### SUMMARIZATION ####
+// #######################
+
+
+var storeSummaryInObj = function(data) {
+	
+	var result = data.result;
+	
+	for (var i = 0; i < result.news.length; i++) {
+		var title = result.news[i].source.enriched.url.title;
+		var content = result.news[i].source.enriched.url.text;
+	
+		summarize(title, content, function(err, summary) {
+			result.news[i].source.summary = {};
+			result.news[i].source.summary.summary = summary;
+			result.news[i].source.summary.ratio = (100 - (100 * (summary.length / (title.length + content.length))));
+		});
+	}
+}
+
+
+
 
 // ######################
 // #### SOCKETS: I/O ####
@@ -90,6 +146,7 @@ $.get("/getSocketUrl", function (dat) {
 		$('#newsSource').text("Source of news result: " + result.sourceFile);
 		console.log("getNewsResult");
 		console.log(data);
+		storeSummaryInObj(data);
 		createNewsTable(result);
 		// console.log(JSON.stringify(result,null,4));
 		// $("<p>",{ text:JSON.stringify(result,null,4)}).appendTo($('body'))
@@ -140,24 +197,3 @@ var afinnColorText = function (text, wordScores) {
 // #### MISCELLANEOUS ####
 // #######################
 
-
-//simple variable passing between client and server without client ID
-// $.get("/getvar", function (data) {
-// console.log(JSON.stringify(data,null,4));
-// });
-
-
-// var getNews = function () {
-// var data = {
-// queryString: $('#queryString').val()
-// };
-// $.ajax({
-// url: "/getNews",
-// data: data,
-// success: function (data) {
-// name = data.name;
-// console.log(name);
-// },
-// dataType: "json"
-// });
-// }
